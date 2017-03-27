efb367b10d7cef5a7632eb84c79f8a5c43955e6d
Vladimir Zhigalo <Zhigalo.V.Y@gmail.com>
2017-03-27 04:49:33 +0300

    realise

--- lab3/lb3_BWT/lb3_BWT/Form1.Designer.cs
+++ lab3/lb3_BWT/lb3_BWT/Form1.Designer.cs
@@ -28,10 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.buttonClear = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -41,7 +44,6 @@
             this.richTextBox1.Size = new System.Drawing.Size(317, 96);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.BWT);
             // 
             // richTextBox2
             // 
@@ -69,17 +71,43 @@
             this.buttonClear.UseVisualStyleBackColor = true;
             this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 347);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(178, 347);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "<-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(341, 357);
+            this.ClientSize = new System.Drawing.Size(341, 384);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonClear);
             this.Controls.Add(this.richTextBox3);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "lb3 BWT";
+            this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
@@ -91,6 +119,8 @@
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.RichTextBox richTextBox3;
         private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- lab3/lb3_BWT/lb3_BWT/Form1.cs
+++ lab3/lb3_BWT/lb3_BWT/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace lb3_BWT
@@ -22,50 +16,49 @@ namespace lb3_BWT
 
         }
 
-        private void BWT(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            if (richTextBox1.Text.Length != 0)
+            string[] text = new string[richTextBox1.TextLength];
+            text[0] = richTextBox1.Text;
+
+            for (int i = 1; i < richTextBox1.TextLength; i++)
             {
-                string[] text = new string[richTextBox1.TextLength];
-                text[0] = richTextBox1.Text;
-                for (int i = 1; i < richTextBox1.TextLength; i++)
-                {
-                    text[i] = text[i - 1];
-                    string last = text[i].Last().ToString();
-                    text[i] = text[i].Remove(text[i].Length - 1).Insert(0, last);
-                }
-                Array.Sort(text);
-                int index = Array.IndexOf(text, richTextBox1.Text);
-
-                richTextBox2.Text = string.Empty;
-                for (int i = 0; i < text.Length; i++)
-                    richTextBox2.Text += text[i].Last();
-                richTextBox2.Text += '|' + index.ToString();
-
-
-                // Расшифровка
-                string text2S = richTextBox2.Text;
-                string[] par = text2S.Split('|');
-                if (par.Length != 2)
-                    MessageBox.Show("Error");
-
-                string[] matrix2 = new string[par[0].Length];
-                string text2 = par[0];
-
-                for (int i = 0; i < matrix2.Length; i++)
-                    matrix2[i] = text2[i].ToString();
-
-                Array.Sort(matrix2);
-                for (int j = 1; j < matrix2.Length; j++)
-                {
-                    for (int i = 0; i < matrix2.Length; i++)
-                    {
-                        matrix2[i] = matrix2[i].Insert(0, text2[i].ToString());
-                    }
-                    Array.Sort(matrix2);
-                }
-                richTextBox3.Text = matrix2[int.Parse(par[1])];
+                text[i] = text[i - 1];
+                string last = text[i].Last().ToString();
+                text[i] = text[i].Remove(text[i].Length - 1).Insert(0, last);
             }
+
+            Array.Sort(text);
+            int index = Array.IndexOf(text, richTextBox1.Text);
+
+            for (int i = 0; i < text.Length; i++)
+                richTextBox2.Text += text[i].Last();
+
+            richTextBox2.Text += '/' + index.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Расшифровка
+            string text2S = richTextBox2.Text;
+            string[] par = text2S.Split('/');
+            if (par.Length != 2)
+                MessageBox.Show("Error");
+
+            string[] matrix2 = new string[par[0].Length];
+            string text2 = par[0];
+
+            for (int i = 0; i < matrix2.Length; i++)
+                matrix2[i] = text2[i].ToString();
+
+            Array.Sort(matrix2);
+            for (int j = 1; j < matrix2.Length; j++)
+            {
+                for (int i = 0; i < matrix2.Length; i++)
+                    matrix2[i] = matrix2[i].Insert(0, text2[i].ToString());
+                Array.Sort(matrix2);
+            }
+            richTextBox3.Text = matrix2[int.Parse(par[1])];
         }
 
         private void buttonClear_Click(object sender, EventArgs e)
