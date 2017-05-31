cc4ed539798f3a6303843cc475fe7602865a8a3b
Vladimir Zhigalo <Zhigalo.V.Y@gmail.com>
2017-05-31 20:32:19 +0300

    release

---
+++ lab8/Antivirus/Antivirus/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Antivirus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scanButton = new System.Windows.Forms.Button();
+            this.virusListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // scanButton
+            // 
+            this.scanButton.Location = new System.Drawing.Point(12, 152);
+            this.scanButton.Name = "scanButton";
+            this.scanButton.Size = new System.Drawing.Size(204, 23);
+            this.scanButton.TabIndex = 1;
+            this.scanButton.Text = "Сканировать";
+            this.scanButton.UseVisualStyleBackColor = true;
+            this.scanButton.Click += new System.EventHandler(this.scanButton_Click);
+            // 
+            // virusListBox
+            // 
+            this.virusListBox.FormattingEnabled = true;
+            this.virusListBox.Location = new System.Drawing.Point(12, 12);
+            this.virusListBox.Name = "virusListBox";
+            this.virusListBox.Size = new System.Drawing.Size(204, 134);
+            this.virusListBox.TabIndex = 3;
+            this.virusListBox.DoubleClick += new System.EventHandler(this.virusListBox_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 193);
+            this.Controls.Add(this.virusListBox);
+            this.Controls.Add(this.scanButton);
+            this.Name = "Form1";
+            this.Text = "Antivirus";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button scanButton;
+        private System.Windows.Forms.ListBox virusListBox;
+    }
+}
+
---
+++ lab8/Antivirus/Antivirus/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Antivirus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void scanButton_Click(object sender, EventArgs e)
+        {
+            var processessList = Process.GetProcesses();
+
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    findProgressBar.Value = i;
+            //    Thread.Sleep(20);
+            //}
+
+            foreach (var item in processessList)
+            {
+                if (item.ProcessName == "virus" /*|| item.ProcessName == "Antivirus"*/)
+                    virusListBox.Items.Add(item.Id + ":" + item.ProcessName);
+            }
+            if (virusListBox.Items.Count != 0)
+            {
+                virusListBox.ForeColor = Color.Red;
+            }
+            else
+                MessageBox.Show("Вирусов не обнаружено");
+        }
+
+        private void virusListBox_DoubleClick(object sender, EventArgs e)
+        {
+            string str = virusListBox.SelectedItem.ToString();
+            var tmpArr = str.Split(':');
+
+            // var arr = virusListBox.SelectedIndex.ToString().Split(':', '1');
+            // Process virusProcess = Process.GetProcessesByName(virusListBox.SelectedItem.ToString())[0];
+            Process virusProcess = Process.GetProcessById(Convert.ToInt32(tmpArr[0]));
+            MessageBox.Show(tmpArr[0]);
+            virusProcess.Kill();
+
+            virusListBox.Items.RemoveAt(0);
+            if (virusListBox.Items.Count == 0)
+                virusListBox.BackColor = Color.White;
+        }
+    }
+}
